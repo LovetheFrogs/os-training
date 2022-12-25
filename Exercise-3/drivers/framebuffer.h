203aec2fd8e85c7c376955d31c9c17691731801a
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-12-25 17:18:07 +0100

    Moved drivers to folder

---
+++ Exercise-3/drivers/framebuffer.h
@@ -0,0 +1,50 @@
+#ifndef INCLUDE_FRAMEBUFFER_H
+#define INCLUDE_FRAMEBUFFER_H
+
+#include "io.h"
+
+/* Define all colors to allow for easier reading */
+#define FB_BLACK         0
+#define FB_BLUE          1
+#define FB_GREEN         2
+#define FB_CYAN          3
+#define FB_RED           4
+#define FB_MAGENTA       5
+#define FB_BROWN         6
+#define FB_LIGHT_GREY    7
+#define FB_DARK_GREY     8
+#define FB_LIGHT_BLUE    9
+#define FB_LIGHT_GREEN   10
+#define FB_LIGHT_CYAN    11
+#define FB_LIGHT_RED     12
+#define FB_LIGHT_MAGENTA 13
+#define FB_LIGHT_BROWN   14
+#define FB_WHITE         15
+
+/* Define the I/O ports */
+#define FB_COMMAND_PORT 0x3D4
+#define FB_DATA_PORT    0x3D5
+
+/* Define the I/O port commands */
+#define FB_HIGH_BYTE_COMMAND 14
+#define FB_LOW_BYTE_COMMAND  15
+
+/* Number of columns and rows of the framebuffer */
+#define NUM_OF_ROWS 25
+#define NUM_OF_COLS 80
+
+/* TO_POSITION transform a pair (row, col) to a position i wich can be used by fb_write_cell method */
+#define TO_POSITION(row, col) ((col + (row * NUM_OF_COLS)) * 2)
+
+/* Reference to framebuffer start memory position */
+static char *fb = (char *) 0x000B8000;
+
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
+
+void fb_move_cursor(unsigned short pos);
+
+int fb_write(char *buf, unsigned int len);
+
+void fb_clear();
+
+#endif /* FRAMEBUFFER_H */
