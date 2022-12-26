f79962f86c970a731a3863cbcdf74d4da7b35aff
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-12-26 18:34:41 +0100

    Updated os

--- Exercise-4/drivers/io.s
+++ Exercise-4/drivers/io.s
@@ -16,5 +16,5 @@ outb:
 ;        [esp    ] the return address
 inb:
     mov dx, [esp + 4]       ; move the address of the I/O port to the dx register
-    in al, dx               ; read a byte fro, the I/O port and store it in the al register
+    in  al, dx              ; read a byte from the I/O port and store it in the al register
     ret                     ; return the read byte
--- Exercise-4/kmain.c
+++ Exercise-4/kmain.c
@@ -1,14 +1,18 @@
 #include "drivers/framebuffer.h"
 #include "drivers/serialport.h"
+#include "segmentation/segmentation.h"
 
 void kmain()
 {
     char buf[] = "This is lovethefrogs' OS.";
     char res[1];
     fb_clear();
+
     res[0] = fb_write(buf, sizeof(buf)) + 49;
     serial_write(SERIAL_COM1_BASE, res, sizeof(res));
 
     char buff[] = "Check the out log to see result of previous print!";
     fb_write(buff, sizeof(buff));
+
+    create_gdt();
 }
