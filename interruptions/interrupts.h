bd7a370f05c7a09d6169ec14f60eab781d8e06e2
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-12-26 22:35:09 +0100

    Implemented DS
    Implemented custom data structures idtDescriptor and idtEntry

--- interruptions/interrupts.h
+++ interruptions/interrupts.h
@@ -1,6 +1,31 @@
 #ifndef INCLUDE_INTERRUPTS_H
 #define INCLUDE_INTERRUPTS_H
 
+/** idtDescriptor is a data structure used to contain the start address and size of the
+ *  Interrupt Descriptor Table. It simulates the following bit configuration:
+ * 
+ *          Bit: | 47 - 32 | 31 - 0  |
+ *      Content: |  size   | address |
+ */
+struct idtDescriptor
+{
+    unsigned short size;
+    unsigned int address;
+} __attribute__((packed));
 
+/** idtEntry is a data structure used to contain the values inside an Entry of the 
+ *  Interrupt Descriptor Table. It simulates the following bit configuration:
+ * 
+ *          Bit: | 63       48 | 47 | 46 45 | 44 | 43 | 42 41 40 | 39 38 37 | 36 35 34 32 | 31            16 | 15       0 |
+ *      Content: | offset high | P  | DPL   | 0  | D  | 1  1  0  | 0  0  0  | reserved    | segment selector | offset low |
+*/
+struct idtEntry
+{
+    unsigned short offset_l;
+    unsigned short kernel_cs;
+    unsigned char reserved;
+    unsigned char atributes;
+    unsigned short offset_h
+} __attribute__((packed));
 
 #endif /* INCLUDE_INTERRUPTIONS_H */
