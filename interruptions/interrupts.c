ae9f9d337dd4b60ef136c47b79ae9d87a0e584ee
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-12-28 11:58:09 +0100

    added header of interrup_hadler function

--- interruptions/interrupts.c
+++ interruptions/interrupts.c
@@ -1,3 +1,8 @@
 #include "interrupts.h"
 
 static struct idtEntry descriptors[NUM_OF_SIGNALS];
+
+void interrupt_handler(__attribute__((unused)) struct cpuState reg_state, __attribute__((unused)) struct stackState stack_state, unsigned int inr_code)
+{
+
+}
