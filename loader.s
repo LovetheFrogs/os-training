fd2dec378013f25942348e73151e3f4bfa1cdab9
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2023-01-25 17:30:57 +0100

    Update loader.s

--- loader.s
+++ loader.s
@@ -2,11 +2,10 @@ extern kmain			    	                ; extern function sum_of_three declared in k
 global loader
 
 MAGIC_NUMBER    equ 0x1BADB002              	; define the magic number constant
-FLAGS           equ 0x0                     	; multiboot flags
 ALIGN_MODULES   equ 0x00000001                  ; tell GRUB to align modules
 
 ; calculate the checksum (all options + checksum should be 0)
-CHECKSUM        equ -(MAGIC_NUMBER + FLAGS + ALIGN_MODULES)
+CHECKSUM        equ -(MAGIC_NUMBER + ALIGN_MODULES)
 
 KERNEL_STACK_SIZE equ 4096                      ; size of stack (4 KB)
 
@@ -18,11 +17,11 @@ section .bss
 section .text
 align 4                                     	; 4 byte aligning the code
     dd MAGIC_NUMBER                         	; write magic_number
-    dd FLAGS                                	; flags
     dd ALIGN_MODULES                            ; write the align modules instruction
     dd CHECKSUM                                 ; and checksum to the machine code
     
 loader:
+    push ebx                                    ; push contens of ebx register to stack
     call kmain                                  ; call kmain, result will be in eax
     
 .loop:
